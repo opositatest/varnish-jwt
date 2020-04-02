f44f608a64daa67c55705fbedc6e96a7c6329f3d
Adan Rivas <arivasloureiro@gmail.com>
2020-04-02 13:51:53 +0200

    New test to check json syntax in response

---
+++ varnish/test/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TOKEN='aaaeyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJlIjoiOkJERTEwMDAzNTE6IiwiZXhwIjoyMDM0NDMyMzYyLCJzdWIiOiJodHRwX3Rlc3QifQ.rsJrHXD0LIH-rt1_kicGb6xAJK0-zLAQ-kRJfAYQNivOcIrpqIugjmmoDm2f-tGjcu5n7PrcYm4HHIN3X_mLZw'
+
+varnishd -f /etc/varnish/default.vcl &
+apt update && apt install --no-install-recommends -y jq
+
+result="$(curl -s -H 'Authorization: Bearer '$TOKEN'' -X GET localhost -H  'accept: application/json')"
+
+jq -e . >/dev/null <<<"$result"
